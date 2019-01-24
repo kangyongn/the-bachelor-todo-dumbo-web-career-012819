f37167beb45f9a3c03af6f21cf5c27dd382d484e
Yong Nam Kang <j.yongnamkang@gmail.com>
2019-01-24 23:32:20 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -22,7 +22,9 @@ def count_contestants_by_hometown(data, hometown)
 end
 
 def get_occupation(data, hometown)
-  # code here
+  data.each do |seasons, contestants|
+    contestants.each {|individual| return individual["occupation"] if individual["hometown"] == hometown}
+  end
 end
 
 def get_average_age_for_season(data, season)
